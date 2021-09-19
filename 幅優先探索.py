920ca66deb4a42c5a1739497f5bb145163229c0e
Matsumoto213 <matsumoto2171205@gmail.com>
2021-09-19 23:57:58 +0900

    9月19日午後の更新

---
+++ 幅優先探索.py
@@ -0,0 +1,30 @@
+# abc007(BFS)
+
+from collections import deque
+
+R, C = map(int, input().split())
+sy, sx = map(int, input().split())
+gy, gx = map(int, input().split())
+
+sy, sx, gy, gx = sy - 1, sx - 1, gy - 1, gx - 1
+c = [[c for c in input()] for _ in range(R)]
+visited = [[-1]* C for _ in range(R)]
+
+def bfs(sy,sx,gy,gx,c,visited):
+    visited[sy][sx] = 0
+    Q = deque([])
+    Q.append([sx,sy])
+
+    while Q:
+        y,x = popleft()
+
+        if [y, x] == [gy, gx]:
+            return visited[y][x]
+
+        for i, j in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+            if c[y + 1][x + j] == "." and visited[y + i][x + j] == -1:
+                # 探索可能かつ未探索の場合
+                visited[y + i][x + j] = visited[y][x] + 1
+                Q.append([y + i, x + j])
+print(bfs(sy, sx, gy, gx, c, visited))
+        
