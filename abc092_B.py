4d167416ece2b8f676827f2eb995fb0c51b734ce
yusuke <yusuke.213@icloud.com>
2021-09-21 23:19:41 +0900

    9月21日午後の更新

--- abc092_B.py
+++ abc092_B.py
@@ -1,4 +1,18 @@
 n = int(input())
 d, x  = map(int, input().split())
 A = [int(input()) for i in range(n)]
+lst = [1] * n
 
+count = len(A)
+i = 0
+
+while True:
+    if i == len(A) - 1:
+        break
+    if lst[i] > d:
+        i += 1
+    else:
+        count += 1
+    lst[i] += A[i]
+
+print(count + x)
