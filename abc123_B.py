4abea84e96909d088869fd2eb8972739c657e7ba
yusuke <yusuke.213@icloud.com>
2021-09-23 11:55:51 +0900

    9月23日午前の更新

--- abc123_B.py
+++ abc123_B.py
@@ -11,16 +11,30 @@ d = int(input())
 e = int(input())
 
 lst = [a,b,c,d,e]
+least = 10
+result = 0
 
-min_least = 10
+for idx,i in enumerate(lst):
+    i = str(i)
+    hkt = int(i[-1])
+    if hkt != 0:
+        least = min(least,hkt)
+
+for i,l in enumerate(lst):
+    l = str(l)
+    least = str(least)
+    if l[-1] == least:
+        result = i + 1
+        break
+
 ans = 0
+for id,j in enumerate(lst):
+    j = str(j)
+    hitoketame = int(j[-1])
+    if id != result - 1 and hitoketame != 0:
+        tasu = 10 - hitoketame
+        j = int(j)
+        j += tasu
+    ans += int(j)
 
-for i in lst:
-    st = str(i)
-    least = int(st[-1])
-    if least > 0:
-        k = 10 - least
-        i += k
-        min_least = min(least,min_least)
-    ans += i
 print(ans)
