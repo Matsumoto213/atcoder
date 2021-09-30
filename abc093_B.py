eafe90cee602842b4ae08d81b155fb9c663978d1
yusuke <yusuke.213@icloud.com>
2021-09-30 12:21:09 +0900

    9月30日午前の更新

--- abc093_B.py
+++ abc093_B.py
@@ -1,4 +1,10 @@
-a,b k = map(int, input().split())
+a,b,k = map(int,input().split())
 lst = [i for i in range(a, b + 1)]
 
+ans = lst[:k]+lst[b - a - k + 1:]
 
+ans = list(set(ans))
+ans.sort()
+
+for i in ans:
+  print(i)
