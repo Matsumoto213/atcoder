31c898f604e0e4ecccdd08f788a8d79abfc3abaf
yusuke <yusuke.213@icloud.com>
2021-10-19 11:59:03 +0900

    10月19日午前の更新

--- abc135_C.py
+++ abc135_C.py
@@ -2,8 +2,6 @@ n = int(input())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
-a = a[::-1]
-b = b[::-1]
 
 ans = 0
 
@@ -15,23 +13,25 @@ while True:
     if a[n] == 0 or b[n - 1] == 0:
         break
     dif = abs(i - j)
-    if dif < 2:
-        if a[i] < b[j]:
-            b[j] = abs(a[i] - b[i])
-            cnt += a[i]
-            a[i] = 0
-            i += 1
-        elif a[i] == b[j]:
-            cnt += a[i]
-            a[i] = 0
-            b[j] = 0
-            i += 1
-            j += 1
+    if dif <= 2:
+        if i >= j:
+            if a[i] < b[j]:
+                b[j] = abs(a[i] - b[j])
+                cnt += a[i]
+                a[i] = 0
+                i += 1
+            elif a[i] == b[j]:
+                cnt += a[i]
+                a[i] = 0
+                b[j] = 0
+                i += 1
+                j += 1
+            else:
+                a[i] = abs(a[i] - b[j])
+                cnt += b[j]
+                b[j] = 0
+                j += 1
         else:
-            a[i] = abs(a[i] - b[j])
-            cnt += b[j]
-            b[j] = 0
-            j += 1
-        print(a,b,cnt)
+            i = j
 print(cnt)
 
