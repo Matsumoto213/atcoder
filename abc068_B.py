8000597ef395683bd78bea9b3fb61381a00caf21
Matsumoto213 <matsumoto2171205@gmail.com>
2021-10-21 23:21:02 +0900

    10月21日午後の更新

--- abc068_B.py
+++ abc068_B.py
@@ -1,6 +1,17 @@
 n = int(input())
-
-ans = 0
-
-for i in range(1,n + 1):
-    
+lst = []
+ 
+for i in range(1, n + 1):
+    num = i
+    count = 0
+    while True:
+        if num % 2 != 0:
+            lst.append(count)
+            break
+        else:
+            num = num / 2
+            count +=1
+ 
+max_value = max(lst)
+max_index = lst.index(max_value) + 1
+print(max_index)
