392c880f9d696c57ed1fd4c4d025a30c270d6aca
yusuke <yusuke.213@icloud.com>
2021-10-26 10:28:33 +0900

    10月26日午前の更新

---
+++ bit_all.py
@@ -0,0 +1,25 @@
+from itertools import product
+
+# N, W = map(int, input().split())
+# A = list(map(int, input().split()))
+
+N, W = 5,10
+A = [1,2,3,6,8]
+ans = 0
+
+# i番目がA[i]を使って合計にたす
+
+def judge(pro):
+    S = 0
+    for i in range(N):
+        if pro[i]:
+            S += A[i]
+    if S == W:
+        return 1
+    else:
+        return 0
+
+
+for pro in product((0,1), repeat = N):
+    ans += judge(pro)
+print(ans)
