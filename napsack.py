f22936582c8f138b8e884dc1e5b20db49cd74942
Matsumoto213 <matsumoto2171205@gmail.com>
2021-10-26 10:28:33 +0900

    10月26日午前の更新

--- napsack.py
+++ napsack.py
@@ -1,13 +1,22 @@
 N,W = map(int, input().split())
+w = []
+v = []
+
 
 dp = [[0] * (W + 1) for _ in range(N + 1)]
 
+for i in range(N):
+    x,y = map(int,input().split())
+    w.append(x)
+    v.append(y)
 
-for i in range(n):
-    dp[i + 1] = dp[i].copy()
-    w,v = map(int, input().split())
-    for j in range(W + 1 - w):
-        dp[i + 1][j + w] = max(dp[i][j] + v,dp[i][j + w])
-print(dp)
-print(dp[-1][-1])
+for i in range(N):
+    for j in range(W + 1):
+        # 選ばない時
+        if j < w[i]:
+            dp[i + 1][j] = dp[i][j]
+        # 選ぶ時
+        else:
+            dp[i + 1][j] = max(dp[i][j],dp[i][j - w[i]] + v[i])
+
 
