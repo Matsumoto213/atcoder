7be140609552559b6ed586ec939328ec7903bb0a
yusuke <yusuke.213@icloud.com>
2021-11-04 12:26:04 +0900

    11月4日午前の更新

--- abc_hhkb_2020_C.py
+++ abc_hhkb_2020_C.py
@@ -1,4 +1,16 @@
 n = int(input())
 p = list(map(int, input().split()))
 
-a = []
+ans = 0
+lst = set()
+
+for i in p:
+    lst.add(i)
+    while True:
+        if ans in lst:
+            ans += 1
+        else:
+            print(ans)
+            break
+
+
