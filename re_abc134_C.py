0412888e14a587f05e7d7eb87aaad4d3fd5c9c57
Matsumoto213 <matsumoto2171205@gmail.com>
2021-12-02 17:51:32 +0900

    素晴らしいアルゴリズムに出会えた！！！

--- re_abc134_C.py
+++ re_abc134_C.py
@@ -1,10 +1,12 @@
 # 二重ループをどう回避するか？
 # Numpyで特定のindex以外を抽出する
+# お見事！このような数学が苦手な人にも理解できて簡単なコードこそ至高
 N = int(input())
 A = [int(input()) for _ in range(N)]
+B = sorted(A, reverse = True)
 
-for idx,a in enumerate(A):
-    idx += 1
-    A.pop(idx - 1)
-    print(max(A))
-    A.insert(idx - 1, a)
+for a in A:
+    if a == B[0]:
+        print(B[1])
+    else:
+        print(B[0])
