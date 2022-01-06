0555a7067188bd8a6bf25bf2f8f1ffa5be5c0cd8
yusuke <yusuke.213@icloud.com>
2022-01-06 11:35:37 +0900

    1月6日午前の更新

--- abc168_c.py
+++
@@ -1,4 +0,0 @@
-a,b,h,m = map(int, input().split())
-# 短針の長さがacm　長針の長さがbcm
-# h時m分になった時にどれだけ離れているか？
-
---
+++ dfs.py
@@ -0,0 +1,36 @@
+import sys
+sys.setrecursionlimit(10000)
+
+# 入力の読み込み
+N, M = map(int, input().split())
+G = [[] for _ in range(N)]
+
+# G[i] は都市iから道路で直接繋がっている都市のリスト
+for _ in range(M):
+    A, B = map(int, input().split())
+    # Pythonではindex0始まりなので、都市iを-1して揃える
+    G[A-1].append(B-1)
+print(G)
+def dfs(v):
+    if seen[v]:
+        return
+    
+    # 初期値でスタートをゴール地として設定 ex)(1, 1)をTrue
+    seen[v] = True
+    
+    for vv in G[v]:
+        dfs(vv)
+
+ans = 0
+
+for i in range(N):
+    # チェッカー
+    seen = [False] * N
+    
+    # seen[j]は都市jに到達可能かどうかを表す
+    dfs(i)
+    
+    ans += sum(seen)
+print(ans)
+    
+    
