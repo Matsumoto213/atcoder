e73eea234a8f04aa2f53a86572c5aef6f28db03b
yusuke <yusuke.213@icloud.com>
2022-01-08 22:03:08 +0900

    1月8日

---
+++ abc234_A.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+ft = (t ** 2) + (2 * t) + 3
+ft_t = t + ft
+f_ft_t= (ft_t ** 2) + (2 * ft_t) + 3 
+
+f_ft = (ft ** 2) + (2 * ft) + 3
+temp = f_ft_t + f_ft
+ans = (temp ** 2) + (2 * temp) + 3
+
+print(ans)
