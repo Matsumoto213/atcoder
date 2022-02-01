36d67fedca387b3179ff03a7c7c321779ff12061
yusuke <yusuke.213@icloud.com>
2022-02-01 22:03:54 +0900

    2月1日

--- abc236A.py
+++ abc236A.py
@@ -1,3 +1,12 @@
 S = input()
 a,b = map(int,input().split())
-S[a - 1] = S[b - 1]
+
+for i,key in enumerate(S):
+    i += 1
+    if i == a:
+        print(S[b - 1],end = "")
+    elif i == b:
+        print(S[a - 1], end= "")
+    else:
+        print(key,end = "")
+print()
