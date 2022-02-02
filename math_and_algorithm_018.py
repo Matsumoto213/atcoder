1a01bbcc505b64efb06025d10ce0f8d047c05026
Matsumoto213 <matsumoto2171205@gmail.com>
2022-02-03 08:59:39 +0900

    2月3日

--- math_and_algorithm_018.py
+++ math_and_algorithm_018.py
@@ -1,19 +1,17 @@
 N = int(input())
 A = list(map(int, input().split()))
+a,b,c,d = 0,0,0,0
 
-def calc(bit):
-    temp = 0
-    for i in range(N):
-        if bit & (1 << i):
-            temp += A[i]
-    
-    return temp
+for x in A:
+    if x == 100:
+        a += 1
+    elif x == 200:
+        b += 1
+    elif x == 300:
+        c += 1
+    else:
+        d += 1
 
-ans = 0
-for bit in range(1 << N):
-    tem = calc(bit)
-    print(tem)
-    if tem == 500:
-        ans += 1
+ans = a * d + b * c
 
 print(ans)
