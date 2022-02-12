65f8e196781b23ed832df11972e12dd9f229b00f
yusuke <yusuke.213@icloud.com>
2022-02-12 10:02:18 +0900

    2月12日

---
+++ abc177_C.py
@@ -0,0 +1,17 @@
+N = int(input())
+A = list(map(int, input().split()))
+mod = (10 ** 9) + 7
+su = sum(A)
+
+ans = 0
+
+for i in range(len(A)):
+    su -= A[i]
+    ans += su * A[i]
+    
+    ans %= mod
+
+print(ans)
+    
+    
+
