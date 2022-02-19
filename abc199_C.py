59fc585fe407466b11a186ebc265673d7fa35950
松本友介 <matsumotoyuusuke@matsumotoyuusukenoMacBook-Air-2.local>
2022-02-19 11:01:33 +0900

    2月19日

--- abc199_C.py
+++ abc199_C.py
@@ -1,9 +1,16 @@
 N = int(input())
-S = input()
+S = list(input())
 Q = int(input())
-
 for _ in range(Q):
     t,a,b = map(int, input().split())
 
     if t == 1:
+        temp = S[a - 1]
+        S[a - 1] = S[b - 1]
+        S[b - 1] = temp
+
+    else:
+        S = S[N:] + S[:N]
+print(''.join(S))
+
         
