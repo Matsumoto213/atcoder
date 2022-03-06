b2f632d6ceed30ce6e9cd35375aef05ca8ebe17e
松本友介 <matsumotoyuusuke@matsumotoyuusukenoMacBook-Air-2.local>
2022-03-06 10:02:16 +0900

    3月6日

--- dp_a.py
+++ dp_a.py
@@ -7,5 +7,5 @@ dp[1] = abs(h[0] - h[1])
 
 
 for i in range(2, n):
-    dp[i] = min(dp[i - 1] + (h[i] + h[h - 1]), dp[i - 2] + (h[i]) - h[i -2])
+    dp[i] = min(dp[i - 1] + (h[i] + h[h - 1]), dp[i - 2] + (h[i]) - h[i - 2])
 print(dp[-1])
