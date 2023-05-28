e2e7d3d45d2833c0429c4a4fa1ddc6dce34862b2
松本友介 <matsumoto2171205@gmail.com>
2023-05-28 10:50:42 +0900

    5月20日 abc303の復習

--- abc303/b.py
+++ abc303/b.py
@@ -9,8 +9,10 @@ def find_unfriendly_pairs(N, M, photos):
             larger = max(photo[j], photo[j+1]) - 1
             adjacent[smaller][larger] = 1
     unfriendly_pairs = 0
+    print(adjacent)
     for i in range(N):
         for j in range(i+1, N):
+            print(adjacent[i][j],i,j)
             if not adjacent[i][j]:
                 unfriendly_pairs += 1
     return unfriendly_pairs
