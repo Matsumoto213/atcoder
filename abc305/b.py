1b70215f9a99600de12ef790251b44985f8f48bb
Matsumoto213 <matsumoto2171205@gmail.com>
2023-06-21 23:04:11 +0900

    6月21日 精進

--- abc305/b.py
+++ abc305/b.py
@@ -1,1 +1,8 @@
 p,q = input().split()
+height = {'A':0,'B':3,'C':1,'D':4,'E':1,'F':5,'G':9}
+ans = 0
+start = min(ord(p),ord(q))
+finish = max(ord(p),ord(q))
+for i in range(start + 1,finish + 1):
+    ans += height[chr(i)]
+print(ans)
