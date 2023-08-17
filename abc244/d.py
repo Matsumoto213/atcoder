16cf1c13f429efa06764645c15e42b2f32cef043
Matsumoto213 <matsumoto2171205@gmail.com>
2023-08-17 22:26:40 +0900

    8月17日 精進

---
+++ abc244/d.py
@@ -0,0 +1,7 @@
+S = input().split()
+T = input().split()
+
+if (S[0] == T[0] and S[1] == T[1] and S[2] == T[2]) or (S[0] == T[1] and S[1] == T[2] and S[2] == T[0]) or (S[0] == T[2] and S[1] == T[0] and S[2] == T[1]):
+    print('Yes')
+else:
+    print('No')
