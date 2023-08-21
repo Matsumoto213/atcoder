c50141b3263d1116663d56afff6eecf7056d0e01
Matsumoto213 <matsumoto2171205@gmail.com>
2023-08-21 21:58:10 +0900

    8月20日 精進

---
+++ abc315/ans_e.py
@@ -0,0 +1,46 @@
+def II(): return int(input())
+def MI(): return map(int, input().split())
+def LI(): return list(map(int, input().split()))
+def LISI(): return list(map(int, SI()))
+def LA(f): return list(map(f, input().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+def SI(): return input().strip('\n')
+def MS(): return input().split()
+def LS(): return list(input().strip('\n'))
+def LLS(rows_number): return [LS() for _ in range(rows_number)]
+def gen_matrix(h, w, init): return [[init] * w for _ in range(h)]
+INF = float('inf')
+import sys
+sys.setrecursionlimit(10 ** 7)
+input = sys.stdin.readline
+from bisect import bisect_left, bisect_right
+from heapq import heapify, heappop, heappush
+from math import ceil, floor, log2, log, sqrt, gcd
+from itertools import combinations as comb, combinations_with_replacement as comb_w, product, permutations
+from collections import deque, defaultdict
+from pprint import pprint
+import numpy as np
+from functools import reduce, lru_cache     # decorator: メモ化. max_size=128
+import operator
+
+
+N = II()
+graph = []
+for _ in range(N):
+    c, *p = map(lambda x: int(x) - 1, input().split())
+    graph.append(p)
+
+visited = [False] * N
+ans = []
+
+def dfs(v):
+    if not visited[v]:
+        for u in graph[v]:
+            if visited[u]:
+                continue
+            dfs(u)
+            visited[u] = True
+            ans.append(u + 1)
+
+dfs(0)
+print(*ans)
