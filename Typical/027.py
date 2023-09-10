c0a1a6f1343479eb1d89ea10cdb2268c12d1ff37
Matsumoto213 <matsumoto2171205@gmail.com>
2023-09-10 10:35:59 +0900

    9月10日 精進

---
+++ Typical/027.py
@@ -0,0 +1,34 @@
+INF = float('inf')
+import sys
+sys.setrecursionlimit(10 ** 7)
+input = sys.stdin.readline
+from bisect import bisect_left, bisect_right
+from heapq import heapify, heappop, heappush
+from math import ceil, floor, log2, log, sqrt, gcd
+from itertools import combinations as comb, combinations_with_replacement as comb_w, product, permutations
+from collections import deque, defaultdict
+from pprint import pprint
+import numpy as np
+from functools import reduce, lru_cache     # decorator: メモ化. max_size=128
+import operator
+def II(): return int(input())
+def MI(): return map(int, input().split())
+def LI(): return list(map(int, input().split()))
+def LISI(): return list(map(int, SI()))
+def LA(f): return list(map(f, input().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+def I(): return input()
+def SI(): return input().strip('\n')
+def MS(): return input().split()
+def LS(): return list(input().strip('\n'))
+def LLS(rows_number): return [SI() for _ in range(rows_number)]
+def gen_matrix(h, w, init): return [[init] * w for _ in range(h)]
+
+N = II()
+S = LLS(N)
+
+a = set()
+for i in range(N):
+    if S[i] not in a:
+        a.add(S[i])
+        print(i + 1)
