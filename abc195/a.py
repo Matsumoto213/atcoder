e2e30bf40c184cc809cb49d6646da8d09e4a2962
Matsumoto213 <matsumoto2171205@gmail.com>
2023-09-13 10:10:03 +0900

    9月13日 精進

---
+++ abc195/a.py
@@ -0,0 +1,28 @@
+INF = float('inf')
+import sys
+sys.setrecursionlimit(10 ** 7)
+input = sys.stdin.readline
+from bisect import bisect_left, bisect_right
+from heapq import heapify, heappop, heappush
+from math import ceil, floor, log2, log, sqrt, gcd
+from itertools import combinations as comb, combinations_with_replacement as comb_w, product, permutations
+from collections import deque, defaultdict
+from pprint import pprint
+import numpy as np
+from functools import reduce, lru_cache     # decorator: メモ化. max_size=128
+import operator
+def II(): return int(input())
+def MI(): return map(int, input().split())
+def LI(): return list(map(int, input().split()))
+def LISI(): return list(map(int, SI()))
+def LA(f): return list(map(f, input().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+def I(): return input()
+def SI(): return input().strip('\n')
+def MS(): return input().split()
+def LS(): return list(input().strip('\n'))
+def LLS(rows_number): return [LS() for _ in range(rows_number)]
+def gen_matrix(h, w, init): return [[init] * w for _ in range(h)]
+
+M,H = MI()
+print('Yes' if H % M == 0 else 'No')
