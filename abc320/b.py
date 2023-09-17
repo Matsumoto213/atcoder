4890c0d85c9b65a9dccb464fcfb1d328a1b06660
Matsumoto213 <matsumoto2171205@gmail.com>
2023-09-17 10:52:11 +0900

    9月17日 精進

---
+++ abc320/b.py
@@ -0,0 +1,38 @@
+INF = float('inf')
+import sys
+sys.setrecursionlimit(10 ** 7)
+input = sys.stdin.readline
+from bisect import bisect_left, bisect_right
+from heapq import heapify, heappop, heappush
+from math import ceil, floor, log2, log, sqrt, gcd
+from itertools import combinations as comb, combinations_with_replacement as comb_w, product, permutations
+from collections import deque, defaultdict
+from pprint import pprint
+import numpy as np
+from functools import reduce, lru_cache     # decorator: メモ化. max_size=128
+import operator
+def II(): return int(input())
+def MI(): return map(int, input().split())
+def LI(): return list(map(int, input().split()))
+def LISI(): return list(map(int, SI()))
+def LA(f): return list(map(f, input().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+def I(): return input()
+def SI(): return input().strip('\n')
+def MS(): return input().split()
+def LS(): return list(input().strip('\n'))
+def LLS(rows_number): return [LS() for _ in range(rows_number)]
+def gen_matrix(h, w, init): return [[init] * w for _ in range(h)]
+
+S = I()
+N = len(S)
+
+def is_palindrome(s):
+    return s == s[::-1]
+
+ans = 1
+for i in range(N - 2):
+    for j in range(N - 1,i + 1, -1):
+        if is_palindrome(S[i:j]):
+            ans = max(j - i, ans)
+print(ans)
