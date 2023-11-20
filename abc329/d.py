924257461cd4d0d1ed3f04674effe330bf152e44
Matsumoto213 <matsumoto2171205@gmail.com>
2023-11-21 08:07:00 +0900

    11月21日 精進

---
+++ abc329/d.py
@@ -0,0 +1,46 @@
+INF = float('inf')
+import sys
+sys.setrecursionlimit(10 ** 7)
+input = sys.stdin.readline
+from bisect import bisect_left, bisect_right
+from heapq import heapify, heappop, heappush
+from math import ceil, floor, log2, log, sqrt, gcd
+from itertools import combinations as comb, combinations_with_replacement as comb_w, product, permutations
+from collections import deque, defaultdict
+from pprint import pprint
+import numpy as np
+from functools import reduce, lru_cache     # decorator: メモ化. max_size=128
+import operator
+import re
+d = [(1, 0),(1, 1),(0, 1),(-1, 1),(-1, 0),(-1, -1),(0, -1),(1, -1)]
+def II(): return int(input())
+def MI(): return map(int, input().split())
+def LI(): return list(map(int, input().split()))
+def LISI(): return list(map(int, SI()))
+def LA(f): return list(map(f, input().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+def I(): return input()
+def SI(): return input().strip('\n')
+def MS(): return input().split()
+def LS(): return list(input().strip('\n'))
+def LLS(rows_number): return [LS() for _ in range(rows_number)]
+def gen_matrix(h, w, init): return [[init] * w for _ in range(h)]
+
+N,M = MI()
+A = LI()
+
+dict = defaultdict()
+for i in range(M):
+    dict[A[i]] = 0
+
+ans = A[0]
+
+# 一番投票数の多い番号を常に押さえておく
+for i in range(M):
+    dict[A[i]] += 1
+    if dict[ans] < dict[A[i]]:
+        ans = A[i]
+    
+    if (dict[ans] == dict[A[i]]) and (ans > A[i]):
+        ans = A[i]
+    print(ans)
