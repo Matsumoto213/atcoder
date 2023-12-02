e25ea6fcc386df192fea25f65d1b8b0896d09acb
Matsumoto213 <matsumoto2171205@gmail.com>
2023-12-02 22:37:15 +0900

    12月2日 abc331

---
+++ abc331/e.py
@@ -0,0 +1,222 @@
+INF = float('inf')
+import sys
+sys.setrecursionlimit(10 ** 7)
+input = sys.stdin.readline
+from bisect import bisect_left, bisect_right
+from heapq import heapify, heappop, heappush
+from math import ceil, floor, log2, log, sqrt, gcd
+from itertools import combinations as comb, combinations_with_replacement as comb_w, product, permutations
+from collections import deque, defaultdict, Counter
+from pprint import pprint
+import numpy as np
+from functools import reduce, lru_cache     # decorator: メモ化. max_size=128
+import operator
+import re
+from scipy.special import comb as cmb
+
+"""
+・使い方(個人的まとめ)
+s=SortedSet()
+s.a: SortedSetの中身を返す。
+len(s), x in s, x not in s: リストと同じ要領で使える。
+s.add(x): xを追加してTrueを返す。ただしxがすでにs内にある場合、xは追加せずにFalseを返す。
+s.discard(x): xを削除してTrueを返す。ただしxがs内にない場合、何もせずにFalseを返す。
+s.lt(x): xより小さい最大の要素を返す。もし存在しないなら、Noneを返す。
+s.le(x): x以下の最大の要素を返す。もし存在しないなら、Noneを返す。
+s.gt(x): xより大きい最小の要素を返す。もし存在しないなら、Noneを返す。
+s.ge(x): x以上の最小の要素を返す。もし存在しないなら、Noneを返す。
+s.index(x): xより小さい要素の数を返す。
+s.index_right(x): x以下の要素の数を返す。
+
+・使い方URL
+https://github.com/tatyam-prime/SortedSet
+"""
+from typing import Generic, Iterable, Iterator, List, Tuple, TypeVar, Optional
+T = TypeVar('T')
+class SortedSet(Generic[T]):
+    BUCKET_RATIO = 16
+    SPLIT_RATIO = 24
+    
+    def __init__(self, a: Iterable[T] = []) -> None:
+        "Make a new SortedSet from iterable. / O(N) if sorted and unique / O(N log N)"
+        a = list(a)
+        n = self.size = len(a)
+        if any(a[i] > a[i + 1] for i in range(n - 1)):
+            a.sort()
+        if any(a[i] >= a[i + 1] for i in range(n - 1)):
+            a, b = [], a
+            for x in b:
+                if not a or a[-1] != x:
+                    a.append(x)
+        bucket_size = int(ceil(sqrt(n / self.BUCKET_RATIO)))
+        self.a = [a[n * i // bucket_size : n * (i + 1) // bucket_size] for i in range(bucket_size)]
+
+    def __iter__(self) -> Iterator[T]:
+        for i in self.a:
+            for j in i: yield j
+
+    def __reversed__(self) -> Iterator[T]:
+        for i in reversed(self.a):
+            for j in reversed(i): yield j
+    
+    def __eq__(self, other) -> bool:
+        return list(self) == list(other)
+    
+    def __len__(self) -> int:
+        return self.size
+    
+    def __repr__(self) -> str:
+        return "SortedSet" + str(self.a)
+    
+    def __str__(self) -> str:
+        s = str(list(self))
+        return "{" + s[1 : len(s) - 1] + "}"
+
+    def _position(self, x: T) -> Tuple[List[T], int, int]:
+        "return the bucket, index of the bucket and position in which x should be. self must not be empty."
+        for i, a in enumerate(self.a):
+            if x <= a[-1]: break
+        return (a, i, bisect_left(a, x))
+
+    def __contains__(self, x: T) -> bool:
+        if self.size == 0: return False
+        a, _, i = self._position(x)
+        return i != len(a) and a[i] == x
+
+    def add(self, x: T) -> bool:
+        "Add an element and return True if added. / O(√N)"
+        if self.size == 0:
+            self.a = [[x]]
+            self.size = 1
+            return True
+        a, b, i = self._position(x)
+        if i != len(a) and a[i] == x: return False
+        a.insert(i, x)
+        self.size += 1
+        if len(a) > len(self.a) * self.SPLIT_RATIO:
+            mid = len(a) >> 1
+            self.a[b:b+1] = [a[:mid], a[mid:]]
+        return True
+    
+    def _pop(self, a: List[T], b: int, i: int) -> T:
+        ans = a.pop(i)
+        self.size -= 1
+        if not a: del self.a[b]
+        return ans
+
+    def discard(self, x: T) -> bool:
+        "Remove an element and return True if removed. / O(√N)"
+        if self.size == 0: return False
+        a, b, i = self._position(x)
+        if i == len(a) or a[i] != x: return False
+        self._pop(a, b, i)
+        return True
+    
+    def lt(self, x: T) -> Optional[T]:
+        "Find the largest element < x, or None if it doesn't exist."
+        for a in reversed(self.a):
+            if a[0] < x:
+                return a[bisect_left(a, x) - 1]
+
+    def le(self, x: T) -> Optional[T]:
+        "Find the largest element <= x, or None if it doesn't exist."
+        for a in reversed(self.a):
+            if a[0] <= x:
+                return a[bisect_right(a, x) - 1]
+
+    def gt(self, x: T) -> Optional[T]:
+        "Find the smallest element > x, or None if it doesn't exist."
+        for a in self.a:
+            if a[-1] > x:
+                return a[bisect_right(a, x)]
+
+    def ge(self, x: T) -> Optional[T]:
+        "Find the smallest element >= x, or None if it doesn't exist."
+        for a in self.a:
+            if a[-1] >= x:
+                return a[bisect_left(a, x)]
+    
+    def __getitem__(self, i: int) -> T:
+        "Return the i-th element."
+        if i < 0:
+            for a in reversed(self.a):
+                i += len(a)
+                if i >= 0: return a[i]
+        else:
+            for a in self.a:
+                if i < len(a): return a[i]
+                i -= len(a)
+        raise IndexError
+    
+    def pop(self, i: int = -1) -> T:
+        "Pop and return the i-th element."
+        if i < 0:
+            for b, a in enumerate(reversed(self.a)):
+                i += len(a)
+                if i >= 0: return self._pop(a, ~b, i)
+        else:
+            for b, a in enumerate(self.a):
+                if i < len(a): return self._pop(a, b, i)
+                i -= len(a)
+        raise IndexError
+    
+    def index(self, x: T) -> int:
+        "Count the number of elements < x."
+        ans = 0
+        for a in self.a:
+            if a[-1] >= x:
+                return ans + bisect_left(a, x)
+            ans += len(a)
+        return ans
+
+    def index_right(self, x: T) -> int:
+        "Count the number of elements <= x."
+        ans = 0
+        for a in self.a:
+            if a[-1] > x:
+                return ans + bisect_right(a, x)
+            ans += len(a)
+        return ans
+
+# d = [(1, 0),(1, 1),(0, 1),(-1, 1),(-1, 0),(-1, -1),(0, -1),(1, -1)]
+def II(): return int(input())
+def MI(): return map(int, input().split())
+def LI(): return list(map(int, input().split()))
+def LISI(): return list(map(int, SI()))
+def LA(f): return list(map(f, input().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+def I(): return input()
+def SI(): return input().strip('\n')
+def MS(): return input().split()
+def LS(): return list(input().strip('\n'))
+def LLS(rows_number): return [LS() for _ in range(rows_number)]
+def gen_matrix(h, w, init): return [[init] * w for _ in range(h)]
+
+N,M,L = MI()
+a = LI()
+b = LI()
+excluded_combos = []
+for i in range(L):
+    c,d = MI()
+    excluded_combos.append((c,d))
+
+def find_highest_priced_set_efficient(N, M, main_dishes, side_dishes, excluded_combinations):
+    # 除外された組み合わせのためのセットを作成
+    excluded_set = {(c - 1, d - 1) for c, d in excluded_combinations}
+
+    # 主菜と副菜の価格を降順でソート
+    sorted_main_dishes = sorted(enumerate(main_dishes), key=lambda x: x[1], reverse=True)
+    sorted_side_dishes = sorted(enumerate(side_dishes), key=lambda x: x[1], reverse=True)
+
+    # 最も高価な組み合わせを探す
+    for main_index, main_price in sorted_main_dishes:
+        for side_index, side_price in sorted_side_dishes:
+            if (main_index, side_index) not in excluded_set:
+                # この組み合わせが除外されていなければ、その価格が最高価格
+                return main_price + side_price
+
+    # 全ての組み合わせが除外されている場合（理論上はあり得ないが、念のため）
+    return -1
+
+max_price = find_highest_priced_set_efficient(N, M, a, b, excluded_combos)
+print(max_price)
