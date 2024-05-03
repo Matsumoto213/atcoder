d1a644803d9a40c35884b28b9c3d0c6a850513f6
Matsumoto213 <matsumoto2171205@gmail.com>
2024-05-03 11:45:54 +0900

    5月3日 C++精進

---
+++ C++/APG4b/EX10.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  cout << "A:" ;
+  // ここにプログラムを追記
+  for (int i = 0; i < A; i++) {
+    cout << "]";
+  }
+
+  cout << endl;
+
+  cout << "B:";
+  for (int j = 0; j < B; j++){
+    cout << "]";
+  }
+  cout << endl;
+}
